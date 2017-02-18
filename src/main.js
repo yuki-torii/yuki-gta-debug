7ae2b03cf459ac434699f983525b93b1a2e626b0
limichange <limichange@hotmail.com>
2017-02-18 22:47:51 +0800

    🚧  - Work in progress.

--- src/main.js
+++ src/main.js
@@ -1,3 +1,24 @@
-import Gta from 'yuki-gta'
+import Gta from 'yuki-gta/dist/yuki-gta'
 
-console.log(new Gta())
+const gta = new Gta({
+  google: 'UA-54675108-19',
+  baidu: '417fa9fc2be153b1bec6de7b39208722'
+})
+
+gta.event({
+  page: 'page',
+  action: 'action',
+  label: 'label',
+  value: 1
+})
+
+gta.event({
+  page: 'page',
+  action: 'action',
+  label: 'label',
+  value: 2
+})
+
+gta.pageview('page1')
+gta.pageview('page2')
+gta.pageview('page3')
