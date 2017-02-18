8dd4b8af46e56a357a7931535772f8d9bd7fb4f8
limichange <limichange@hotmail.com>
2017-02-18 21:42:45 +0800

    🔥   - Remove some code

--- src/main.js
+++ src/main.js
@@ -1,11 +1,3 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+import Gta from 'yuki-gta'
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
-})
+console.log(new Gta())
